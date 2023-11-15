091ef0f6f40252d9c8e33775a21b78b1c18edcff
maxischiavello <maxischiavello12@gmail.com>
2023-11-15 17:49:10 -0300

    product controller and products view

---
+++ src/main/java/com/ecommerce/springecommerce/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.ecommerce.springecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping("")
+    public String showProducts() {
+        return "products/show";
+    }
+}
