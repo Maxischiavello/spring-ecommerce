da0011c9c24300e9601eaec221bd3b2afef1923f
maxischiavello <maxischiavello12@gmail.com>
2023-12-01 16:49:47 -0300

    update and delete image functionality from product controller

--- src/main/java/com/ecommerce/springecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/springecommerce/controller/ProductController.java
@@ -41,18 +41,9 @@ public class ProductController {
         product.setUser(user);
 
         // image upload
-        if (product.getId() == null) { // cuando se crea un producto
+        if (product.getId() == null) {
             String imageName = uploadFileService.saveImage(file);
             product.setImage(imageName);
-        } else {
-            if (file.isEmpty()) { // cuando editamos el producto pero no la imagen
-                Product p = new Product();
-                p = productService.getProduct(product.getId()).get();
-                product.setImage(p.getImage());
-            } else {
-                String imageName = uploadFileService.saveImage(file);
-                product.setImage(imageName);
-            }
         }
 
         productService.saveProduct(product);
@@ -72,13 +63,33 @@ public class ProductController {
     }
 
     @PostMapping("/update")
-    public String updateProduct(Product product) {
+    public String updateProduct(Product product, @RequestParam("img") MultipartFile file) throws IOException {
+        Product p = new Product();
+        p = productService.getProduct(product.getId()).get();
+
+        if (file.isEmpty()) {
+            product.setImage(p.getImage());
+        } else {
+            if (!p.getImage().equals("default.jpg")) {
+                uploadFileService.deleteImage(p.getImage());
+            }
+            String imageName = uploadFileService.saveImage(file);
+            product.setImage(imageName);
+        }
+        product.setUser(p.getUser());
         productService.updateProduct(product);
         return "redirect:/products";
     }
 
     @GetMapping("/delete/{id}")
     public String deleteProduct(@PathVariable Integer id) {
+        Product product = new Product();
+        product = productService.getProduct(id).get();
+
+        if (!product.getImage().equals("default.jpg")) {
+            uploadFileService.deleteImage(product.getImage());
+        }
+
         productService.deleteProduct(id);
         return "redirect:/products";
     }
