e755edd923d7be6db590546ea18de796e46c5a59
maxischiavello <maxischiavello12@gmail.com>
2023-12-04 11:18:15 -0300

    Display list of products in the interface

--- src/main/java/com/ecommerce/springecommerce/controller/AdminController.java
+++ src/main/java/com/ecommerce/springecommerce/controller/AdminController.java
@@ -1,14 +1,31 @@
 package com.ecommerce.springecommerce.controller;
 
+import com.ecommerce.springecommerce.model.Product;
+import com.ecommerce.springecommerce.service.ProductService;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private ProductService productService;
+
+    private Logger logger= LoggerFactory.getLogger(AdminController.class);
+
+
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        logger.info("Products: {}", products);
         return "admin/home";
     }
 }
