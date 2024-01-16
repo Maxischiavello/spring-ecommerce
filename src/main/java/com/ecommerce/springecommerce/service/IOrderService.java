bdb409e900905ae64118de8495509e6ec9d248e6
maxischiavello <maxischiavello12@gmail.com>
2024-01-16 18:41:45 -0300

    generate order number functionality

--- src/main/java/com/ecommerce/springecommerce/service/IOrderService.java
+++ src/main/java/com/ecommerce/springecommerce/service/IOrderService.java
@@ -2,6 +2,9 @@ package com.ecommerce.springecommerce.service;
 
 import com.ecommerce.springecommerce.model.Order;
 
+import java.util.List;
+
 public interface IOrderService {
+    List<Order> findAll();
     Order save(Order order);
 }
--- src/main/java/com/ecommerce/springecommerce/service/OrderServiceImp.java
+++ src/main/java/com/ecommerce/springecommerce/service/OrderServiceImp.java
@@ -5,6 +5,9 @@ import com.ecommerce.springecommerce.repository.IOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrderServiceImp implements IOrderService {
 
@@ -12,7 +15,37 @@ public class OrderServiceImp implements IOrderService {
     private IOrderRepository orderRepository;
 
     @Override
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    @Override
     public Order save(Order order) {
         return orderRepository.save(order);
+    }
+
+    public String generateOrderNumber() {
+        int number = 0;
+        String orderNumber = "";
+        List<Order> orders = this.findAll();
+        List<Integer> numbers = new ArrayList<>();
+        orders.stream().forEach(o -> numbers.add(Integer.parseInt(o.getNumber())));
+
+        if (orders.isEmpty()) {
+            number = 1;
+        } else {
+            number = numbers.stream().max(Integer::compare).get();
+            number++;
+        }
+
+        if (number < 10) {
+            orderNumber = "000000000" + String.valueOf(number);
+        } else if (number < 100) {
+            orderNumber = "00000000" + String.valueOf(number);
+        } else if (number < 1000) {
+            orderNumber = "0000000" + String.valueOf(number);
+        }
+
+        return orderNumber;
     }
 }
